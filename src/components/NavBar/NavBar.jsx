27dd4047a6ed650731478f42a947fc5413b3e220
Sophia Seremetis <shseremetis@gmail.com>
2021-01-14 16:18:27 -0500

    change a tags to Links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({ user, handleLogout }) => {
     
       {user ?
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Welcome, {user.name}</a>
+          <div class="navbar-brand" >Welcome, {user.name}</div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -15,12 +15,14 @@ const NavBar = ({ user, handleLogout }) => {
               <li className="nav-item"> <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a> </li>
               <li  className="nav-item" ><Link to="#" className="nav-link">Profile</Link></li>
               <li  className="nav-item" >
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             
+             
+              <Link  class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Trips
-        </a>
+                </Link>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <div class="dropdown-item" href="#">Add Trip</div>
-                  <a class="dropdown-item" href="#">View Trips </a>
+                  <Link class="dropdown-item" href="#">Add Trip</Link>
+                  <Link class="dropdown-item" href="#">View Trips </Link>
         </div>
               </li>
               <li  className="nav-item" ><Link to="/users" className="nav-link">Users</Link></li>
@@ -31,7 +33,7 @@ const NavBar = ({ user, handleLogout }) => {
         </nav>
       :
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Welcome</a>
+      <Link class="navbar-brand" href="#">Welcome</Link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
