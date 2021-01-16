c541a0c3c151543d841eda31180e6764d864cae9
Sophia Seremetis <shseremetis@gmail.com>
2021-01-16 14:13:00 -0500

    stubbed up trips controller

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,5 +1,30 @@
 const Trip = require('../models/trip')
 
 module.exports = {
-   
+   index, 
+   show, 
+   create, 
+   update, 
+   delete: deleteTrip
 }
+
+function index (req, res) {
+
+}
+
+function show (req, res) {
+    
+}
+
+function create (req, res) {
+    
+}
+
+function update (req, res) {
+    
+}
+
+function deleteTrip (req, res) {
+    
+}
+
--- routes/trips.js
+++ routes/trips.js
@@ -2,4 +2,6 @@ const router = require('express').Router()
 const tripsCtrl = require('../controllers/trips')
 const trip = require('../models/trip')
 
+router.get('/', )
+
 module.exports = router
