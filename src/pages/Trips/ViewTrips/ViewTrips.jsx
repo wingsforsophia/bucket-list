05f5daa91d72a9b4902eb7eb4315873e507a1892
bcbell <coleman.britt@gmail.com>
2021-01-15 19:33:00 -0500

    Calendar

--- src/pages/Trips/ViewTrips/ViewTrips.jsx
+++ src/pages/Trips/ViewTrips/ViewTrips.jsx
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ItineraryCards from '../../../components/ItineraryCards/ItineraryCards'
 import './ViewTrips.css'
+import Calendar from '../../../components/DatePicker/DatePicker'
 
 const ViewTrips=({user})=> {
 
@@ -10,9 +11,9 @@ const ViewTrips=({user})=> {
             <>
             <div className='viewTrips'>
                 <h1 key={user.id} className='viewTrips'> {user.name}'s Upcoming & Future Trips</h1>
+                    <Calendar/>
+                    <ItineraryCards />
                 
-                
-
                 
                 </div>
                 
