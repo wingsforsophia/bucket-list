d046dd7f4022cc62e0aed9e1212a1ee150b27fc1
Sophia Seremetis <shseremetis@gmail.com>
2021-01-17 11:01:48 -0500

    item create function added to controller

--- controllers/items.js
+++ controllers/items.js
@@ -1,9 +1,20 @@
 const Item = require('../models/item')
+const Trip = require('../models/trip')
 
 module.exports = {
     create
 }
 
 function create (req, res) {
-    
+   Trip.findById(req.params.id)
+   .then((trip) => {
+       trip.itinerary.push(req.body)
+       trip.save()
+         .then(() => {
+             res.json(trip)
+         })
+   })
+   .catch((err) => {
+    res.json(err);
+  });
 }
