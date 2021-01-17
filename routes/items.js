c0351958503ec3d31b2e56cb829124e5d70a3237
Sophia Seremetis <shseremetis@gmail.com>
2021-01-17 16:14:46 -0500

    where I left off with item delete controller

--- routes/items.js
+++ routes/items.js
@@ -4,5 +4,6 @@ const itemsCtrl = require('../controllers/items')
 router.post('/:id/items', itemsCtrl.create)
 router.get('/:id/items', itemsCtrl.index)
 router.put('/:id/items/:itemId', itemsCtrl.update)
+router.delete('/:id/items/:itemId', itemsCtrl.delete)
 
 module.exports = router
