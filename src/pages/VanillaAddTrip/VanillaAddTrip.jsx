1630d07ea51a5925dbc7ecdeb0be87dff665d655
Sophia Seremetis <shseremetis@gmail.com>
2021-01-18 14:07:02 -0500

    finish vanilla add trip form

--- src/pages/VanillaAddTrip/VanillaAddTrip.jsx
+++ src/pages/VanillaAddTrip/VanillaAddTrip.jsx
@@ -34,9 +34,63 @@ function VanillaAddTrip(props) {
         handleAddTrip(state)
     }
 
-	return (
-		<h3>Add Trip Page</h3>
-	)
+    return (
+        <>
+          <div className="AddTrip">
+            <form className="col s12" ref={formRef} onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="tripName" id="tripName" type="text" className="active" value={state.tripName} onChange={handleChange}/>
+                    <label htmlFor="tripName">Trip Name</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="startDate" id="startDate" type="date" value={state.startDate} onChange={handleChange} required />
+                    <label htmlFor="startDate">Start Date</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="endDate" id="endDate" type="date" value={state.endDate} onChange={handleChange} required/>
+                    <label htmlFor="endDate">End Date</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="departing" id="departing" type="text" value={state.departing} onChange={handleChange}/>
+                    <label htmlFor="departing">Departing from (ex. ORD)</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="arriving" id="arriving" type="text"  value={state.arriving} onChange={handleChange}/>
+                    <label htmlFor="arriving">Arriving at (ex. CDG)</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="city" id="city" type="text" value={state.city} onChange={handleChange}/>
+                    <label htmlFor="city">City</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="country" id="country" type="text"  value={state.country} onChange={handleChange}/>
+                    <label htmlFor="country">Country</label>
+                  </div>
+                </div>
+                <button
+                    type="submit"
+                    disabled={invalidForm}
+                >
+                  
+                  Add Trip
+                </button>                           
+            </form>
+          </div>
+        </>
+      )
 }
 
 export default VanillaAddTrip;
