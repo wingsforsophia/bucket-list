5d7c681938981eb505f1fe4dfdb61a1780afa822
pohern <ohern_p@yahoo.com>
2021-01-18 10:45:49 -0600

    query works!!

--- src/components/POICard/POICard.jsx
+++ src/components/POICard/POICard.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const POICard = (props) => {
   return (
     <>
-      <h3 style={{color: 'black'}}>{props.POI}</h3>
+      <h3 style={{color: 'white'}}>{props.POI}</h3>
       <br />
     </>
   );
