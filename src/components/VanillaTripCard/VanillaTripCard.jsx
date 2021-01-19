a37d5e4d1ca1d8d6ad61572b79b119b4d4adecc4
Sophia Seremetis <shseremetis@gmail.com>
2021-01-19 11:37:50 -0500

    makes trip card

---
+++ src/components/VanillaTripCard/VanillaTripCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function VanillaTripCard() {
+    return(
+        <h3>Trip Card</h3>
+    ) 
+}
+
+export default VanillaTripCard;
