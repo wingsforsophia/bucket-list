eb5d4b81cd7af723e9346f28fc0c82e01f1166ba
Sophia Seremetis <shseremetis@gmail.com>
2021-01-18 19:44:38 -0500

    form persists data to the database

--- src/services/trip-api.js
+++ src/services/trip-api.js
@@ -2,7 +2,7 @@ import tokenService from '../services/tokenService'
 
 
 export function create(trip) {
-    return fetch('/vanilla', {
+    return fetch('/trips', {
           method: "POST",
           headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
           body: JSON.stringify(trip)
@@ -11,7 +11,16 @@ export function create(trip) {
   }
 
 export function getAll(trips) {
-    return fetch ('/vanillatrips', {
+    return fetch ('/trips', {
         mode: 'cors'
     }).then(res => res.json(trips))
 }  
+
+export function deleteTrip(id) {
+    return fetch(`/trips/${id}`, {
+        method: 'DELETE',
+        headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
+    }, {mode: "cors"})
+    .then(res => res.json())
+    
+}
