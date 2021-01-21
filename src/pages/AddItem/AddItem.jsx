bf9dce83278c7fb25140d7c6386ba4ebaabad3e4
Sophia Seremetis <shseremetis@gmail.com>
2021-01-21 12:14:14 -0500

    changes to item model and item form page

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useHistory } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm'
 import * as itemAPI from '../../services/item-api'
+// import Datetime from 'react-datetime'
+// import Calendar from '../../components/Calendar/Calendar'
 
 
 function AddItem(props) {
@@ -10,7 +12,8 @@ function AddItem(props) {
   const formRef = useRef()
 
   const [state, handleChange] = useForm({
-    dateTime: '',
+    date: '',
+    time: '',
     location: '',
     notes: '',
     completed: false,
@@ -31,12 +34,52 @@ function AddItem(props) {
         handleAddItem(state)
     }
 
+    return (
+        <>
+          <div className="AddItem">
+            <form className="col s12" ref={formRef} onSubmit={handleSubmit}>
 
-
-
-    return ( 
-        <h1>hi this is item add page</h1>
-     );
+            <div className="row">
+                  <div className="input-field col s12">
+                    <input name="date" id="date" type="date" value={state.date} onChange={handleChange} required />
+                    <label htmlFor="startDate">Date</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="time" id="time" type="text" value={state.time} onChange={handleChange}/>
+                    <label htmlFor="departing">Time</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="arriving" id="arriving" type="text"  value={state.arriving} onChange={handleChange}/>
+                    <label htmlFor="arriving">Arriving at (ex. CDG)</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="city" id="city" type="text" value={state.city} onChange={handleChange}/>
+                    <label htmlFor="city">City</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input name="country" id="country" type="text"  value={state.country} onChange={handleChange}/>
+                    <label htmlFor="country">Country</label>
+                  </div>
+                </div>
+                <button
+                    type="submit"
+                    disabled={invalidForm}
+                >
+                  
+                  Add Item
+                </button>                           
+            </form>
+          </div>
+        </>
+      )
 }
  
 export default AddItem;
